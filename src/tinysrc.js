6ce47bd06c2430f686a14a380bb017a2298015d6
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:19:53 +0300

    refactor: move tinysrc.js to src directory

---
+++ src/tinysrc.js
@@ -0,0 +1,28 @@
+class tinysrc {
+    constructor(){
+        this.api = "https://tinysrc.me/api/v1"
+        this.headers={"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/73.0.3683.86 Chrome/73.0.3683.86 Safari/537.36","x-requested-with": "xmlhttprequest"}
+    }
+    
+    async req(url, data){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: this.headers,
+                body: JSON.stringify(data),
+            }).then(res => res.json()).then(data => {resolve(data);
+            }).catch(err => reject(err));
+        });
+    }
+    
+    async short_link(link){
+        return (await this.req(`${this.api}/create`, {"url":link,"api_key":"","auth_required":0,"password":"","expiration_time":""}));
+    }
+    async stat_link(prefix,password){
+        return (await this.req(`${this.api}/stat/${prefix}`, {"city":"","password":password}));
+    }
+    async register(username,email,firstname,lastname,password){
+        return (await this.req(`${this.api}/stat/${prefix}`, {"username":username,"email":email,"firstname":firstname,"question":"","answer":"","lastname":lastname,"password":password,"confirm_password":password}));
+    }
+}
+module.exports = {tinysrc};
